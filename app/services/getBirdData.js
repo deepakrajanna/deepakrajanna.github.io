7667bf7b9caead1d458dd8407ffba3a7f8f9d932
Deepak Rajanna <deepakar@gmail.com>
2018-11-11 15:39:26 +0530

    Scientific name
    Added scientific name

--- app/services/getBirdData.js
+++ app/services/getBirdData.js
@@ -1,9 +1,6 @@
 deepakBirdsServices.factory('getBirdData', ['$resource',
   function($resource){
-    /*return $resource('https://spreadsheets.google.com/feeds/list/1DeWM3-DipzuZL5mvMIxG5w_ORqOwWkOCGpfLQqGS54U/od6/public/values?alt=json', {}, {
-      query: {method:'GET', isArray:false}
-    });	
-    */
+    
     return $resource('https://spreadsheets.google.com/feeds/list/1L5o32HKc4lco9aVsO_CWSOzXcQlmRu239qfYg8_CCHM/od6/public/values?alt=json', {}, {
       query: {method:'GET', isArray:false}
     });	
